63e63c8a46953c2333b834bd5ca70bd34efce7dd
Andrey F. Mindubaev <covex.mobile@gmail.com>
2018-05-02 19:52:54 +0300

    doctrine_phpcr.default_session

--- src/Repository/NoneRepository.php
+++ src/Repository/NoneRepository.php
@@ -4,6 +4,11 @@ namespace Doctrine\Bundle\PHPCRBundle\Repository;
 
 class NoneRepository
 {
+    public function login()
+    {
+        return null;
+    }
+
     public function __call($name, $arguments)
     {
         throw new \RuntimeException('Configure real doctrine_phpcr session');
